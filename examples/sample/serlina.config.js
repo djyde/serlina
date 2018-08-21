c6c01361df626569bfc5057b48e1778b16f9c135
Randy <randypriv@gmail.com>
2018-08-21 20:10:39 +0800

    remove devdependencies

--- examples/sample/serlina.config.js
+++ examples/sample/serlina.config.js
@@ -1,19 +1,3 @@
-module.exports = {
-  webpack(webpack, { miniCSSLoader }) {
-    return {
-      module: {
-        rules: [{
-          test: /\.less$/,
-          use: [miniCSSLoader, {
-            loader: 'css-loader' // translates CSS into CommonJS
-          }, {
-            loader: "less-loader",
-            options: {
-              javascriptEnabled: true
-            }
-          }]
-        }]
-      }
-    }
-  }
-}
+const { withAntd } = require('serlina-plugin-antd')
+
+module.exports = withAntd()
